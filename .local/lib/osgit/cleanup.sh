953981307857d53e56fc1dea68d3a0d28df23b22
cristianarbe <c.ariza@posteo.net>
2019-12-22 14:53:12 +0100

    Improve cleanup to delete all temporary files

--- .local/lib/osgit/cleanup.sh
+++ .local/lib/osgit/cleanup.sh
@@ -1,9 +1,13 @@
 cleanup() {
+  test -f "$OSGIT_PROFILE"/packages.current &&
     rm "$OSGIT_PROFILE"/packages.current
+  test -f "$OSGIT_PROFILE"/packages.tmp &&
+    rm "$OSGIT_PROFILE"/packages.tmp
+  test -f "$OSGIT_PROFILE"/packages.tocheckout &&
+    rm "$OSGIT_PROFILE"/packages.tocheckout
 }
 
 clean_exit() {
-    cleanup
-    echo "All done!"
-    exit 0
+  cleanup
+  exit 0
 }
