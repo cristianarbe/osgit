523b773f3dd99abe3dfc4532b8293d625495ad0f
Cristian Ariza <dev@cristianrz.com>
2020-01-19 18:18:20 +0000

    Add more clarity to error message

--- pkgs.h
+++ pkgs.h
@@ -11,14 +11,21 @@ static int
 pkgsclose(char *msg)
 {
 	char cmd[200] = "";
-	int err;
 
-	err = system("dpkg-query -Wf '${Package}=${Version}\n' | sort -n > "
-		     "/home/cariza/.cache/osgit/packages");
-
+	int err = system("dpkg-query -Wf '${Package}=${Version}\n' | sort -n > "
+			 "/home/cariza/.cache/osgit/packages");
 	if (err != 0) {
-		fprintf(stderr, "updating packages");
+		fprintf(stderr, "E: failed updating packages");
 		return 1;
+	}
+
+	int alldone = system(
+	    "git --git-dir=/home/cariza/.cache/osgit/.git --work-tree=/home/cariza/.cache/osgit diff-index --quiet HEAD --");
+
+	printf("alldone is %i\n", alldone);
+
+	if (alldone == 0) {
+		return 0;
 	}
 
 	err = system("git --git-dir=/home/cariza/.cache/osgit/.git "
@@ -36,7 +43,7 @@ pkgsclose(char *msg)
 	(void)strlcat(cmd, "\" > /dev/null 2>&1", sizeof(cmd));
 	err = system(cmd);
 	if (err != 0) {
-		fprintf(stderr, "comitting failed");
+		fprintf(stderr, "E: comitting failed\n");
 		return 1;
 	}
 
@@ -62,11 +69,38 @@ pkgsadd(char *pkgs)
 		return 1;
 	}
 
-	(void)strlcpy(msg, "Add", sizeof(msg));
+	(void)strlcpy(msg, "Add ", sizeof(msg));
 	(void)strlcat(msg, pkgs, sizeof(msg));
 
 	err = pkgsclose(msg);
 	if (err != 0) {
+		return 1;
+	}
+
+	return 0;
+}
+
+int
+pkgsrm(char *params)
+{
+	char msg[100] = "";
+	char cmd[100] = "";
+
+	(void)strlcpy(cmd, "/usr/bin/apt --autoremove purge ", sizeof(cmd));
+	(void)strlcat(cmd, params, sizeof(cmd));
+
+	printf("Running %s\n", cmd);
+	int err = system(cmd);
+	if (err != 0) {
+		fprintf(stderr, "E: error purging\n");
+		return 1;
+	}
+
+	(void)strlcpy(msg, "Remove ", sizeof(msg));
+	(void)strlcat(msg, params, sizeof(msg));
+	err = pkgsclose(msg);
+	if (err != 0) {
+		fprintf(stderr, "E: error closing pkgs\n");
 		return 1;
 	}
 
@@ -89,16 +123,52 @@ pkgsdu()
 int
 pkgslist()
 {
-	char content[2000] = "";
+	FILE *fptr;
+	char c;
 
-	int err = filecpy(
-	    content, "/home/cariza/.cache/osgit/packages", sizeof(content));
-	if (err != 0) {
-		fprintf(stderr, "E: failed reading file contents");
+	// Open file
+	fptr = fopen("/home/cariza/.cache/osgit/packages", "r");
+	if (fptr == NULL) {
+		fprintf(stderr, "E: cannot open file \n");
 		return 1;
 	}
 
-	printf("%s", content);
+	c = fgetc(fptr);
+	while (c != EOF) {
+		printf("%c", c);
+		c = fgetc(fptr);
+	}
+
+	fclose(fptr);
+	return 0;
+}
+
+int
+pkgslog()
+{
+	int err = system(
+	    "git --git-dir=/home/cariza/.cache/osgit/.git --work-tree=/home/cariza/.cache/osgit/ log --oneline");
+	if (err != 0) {
+		fprintf(stderr, "E: error getting log\n");
+		return 1;
+	}
+
+	return 0;
+}
+
+int
+pkgsupgrade()
+{
+	int err = system("/usr/bin/apt update");
+	if (err != 0) {
+		fprintf(stderr, "E: error updating\n");
+		return 1;
+	}
+	err = system("/usr/bin/apt upgrade -y");
+	if (err != 0) {
+		fprintf(stderr, "E: error upgrading\n");
+		return 1;
+	}
 
 	return 0;
 }
