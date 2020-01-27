203e22c7805f568b5836c391f9cbee2e2f9ff322
Cristian Ariza <dev@cristianrz.com>
2020-01-27 06:51:11 +0000

    Change flags

--- config.mk
+++ config.mk
@@ -1,6 +1,6 @@
 CC = clang
 CFLAGS = -fsanitize=signed-integer-overflow -fsanitize=undefined -ggdb3 -O0 \
-	-std=c11 -Wall -W -Werror -Wextra -Wno-sign-compare -Wno-unused-parameter \
-	-Wpointer-arith -Wbad-function-cast -Wno-unused-variable -Wshadow 
+	-std=c11 -Wall -W -Werror -Wextra -Wsign-compare  \
+	-Wpointer-arith -Wbad-function-cast -Wunused-variable -Wshadow 
 PREFIX = /usr/local
 VERSION = v0.7.0
