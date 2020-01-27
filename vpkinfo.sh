ac66e01fa6930fe4cfe4559924c413f26cb95e74
Cristian Ariza <dev@cristianrz.com>
2020-01-27 07:43:15 +0000

    Add shellchecks

--- vpkinfo.sh
+++ vpkinfo.sh
@@ -7,6 +7,8 @@ test -z "$VPKPATH" && echo "VPKPATH is not set" && exit 1
 option="$1"
 shift
 
+# shellcheck disable=SC2048
+# shellcheck disable=SC2086
 case "$option" in
 	"-d") dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n ;;
 	"-g") git --git-dir="$VPKPATH"/.git log --oneline ;;
--- vpkrm.sh
+++ vpkrm.sh
@@ -4,6 +4,8 @@ set -e
 
 test -z "$VPKPATH" && echo "VPKPATH is not set" && exit 1
 
+# shellcheck disable=SC2086
+# shellcheck disable=SC2048
 apt-get -q --autoremove purge $*
 dpkg-query -Wf '${Package}=${Version}\n' | sort > "$VPKPATH"/packages
 git commit -a -m "Remove $*" > /dev/null 2>&1
