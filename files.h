db8eda441ab960ed4d02bcc1cc2febdabda065b3
Cristian Ariza <dev@cristianrz.com>
2020-01-27 06:51:24 +0000

    Add fileappend function

--- files.h
+++ files.h
@@ -18,11 +18,35 @@ filecpy(char *dst, char *path, size_t size)
 	}
 
 	while (i = fgetc(fp), i != EOF) {
-		char c[2] = { i, '\0' };
+		char c[2] = { (char)i, '\0' };
 		(void)strlcat(dst, c, size);
 	}
 
 	(void)fclose(fp);
+
+	return 0;
+}
+
+int
+fileappend(char path[], char buffer[])
+{
+	FILE *pFile = fopen(path, "a");
+	if (pFile == NULL) {
+		fprintf(stderr, "E: pFile is null\n");
+		return 1;
+	}
+
+	int c = fprintf(pFile, "%s", buffer);
+	if (c == 0) {
+		fprintf(stderr, "E: failed printing to pFile\n");
+		return 1;
+	}
+
+	int err = fclose(pFile);
+	if (err != 0) {
+		fprintf(stderr, "E: failed closing file\n");
+		return 1;
+	}
 
 	return 0;
 }
