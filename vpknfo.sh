33cadbbb502fc5662badeacc0964f333ba706823
Cristian Ariza <dev@cristianrz.com>
2020-01-27 12:50:34 +0000

    Change command name

---
+++ vpknfo.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -eu
+
+if test "$#" -eq 0; then
+	cat /var/cache/vpk/packages
+	exit 0
+fi
+
+option="$1"
+shift
+
+# shellcheck disable=SC2048
+# shellcheck disable=SC2086
+case "$option" in
+"-c") git --git-dir /var/cache/vpk/.git show $* ;;
+"-d") dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n ;;
+"-g") git --git-dir=/var/cache/vpk/.git log --oneline ;;
+"-v") apt-cache madison $* | sed 's/ | /=/' ;;
+esac
