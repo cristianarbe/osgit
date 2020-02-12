2a93f301d51ed39a887eff99ca59fc55e92895ab
Cristian Ariza <dev@cristianrz.com>
2020-02-12 12:03:35 +0000

    Prepare vpkrm to update

--- vpkrm.sh
+++ vpkrm.sh
@@ -1,34 +1,79 @@
 #!/bin/sh
-# Copyright 2020 Cristian Ariza. All rights reserved.
+#
+# Copyright 2019 Cristian Ariza
+#
+# See LICENSE file for license details.
+#
+# Uninstalls packages and updates the git repo
 
-# TODO(3): Transform vpkrm to C
+#include pathnames.sh
+#include vpkrmh.sh
 
-set -eu
+main() {
+	while test "$#" -gt 0; do
+		case "$1" in
+		"-v") verbose=true ;;
+		"-d") set -x ;;
+		"--help") print_usg 0 ;;
+		"-c")
+			if test -n "$action"; then
+				print_usg 1
+			fi
 
-if test ! -d /var/cache/vpk/.git; then
-	mkdir -p /var/cache/vpk
-	git --git-dir /var/cache/vpk/.git --work-tree=/var/cache/vpk init
-fi
+			action=checkout
+			shift
+			id="$1"
+			;;
+		"-"*) print_usg 1 ;;
+		*)
+			if test -n "$action"; then
+				print_usg 1
+			fi
 
-dpkg-query -Wf '${Package}=${Version}\n' | sort > /var/cache/vpk/packages
-git --git-dir /var/cache/vpk/.git --work-tree=/var/cache/vpk commit -a -m "Sync" > /dev/null 2>&1 || true
+			action="uninstall"
 
-case "$1" in
-	"-c")
-		# revert
+			packages="$packages $1"
+			;;
+		esac
+
 		shift
+	done
 
-		cp /var/cache/vpk/packages /var/cache/vpk/packages/packages.tmp
-		git --git-dir /var/cache/vpk/.git --work-tree=/var/cache/vpk revert --no-commit "$1"
-		apt-get -q install $(comm -13 /var/cache/vpk/packages.tmp /var/cache/vpk/packages)
-		apt-get -q autoremove $(comm -23 /var/cache/vpk/packages.tmp /var/cache/vpk/packages)
-		git --git-dir /var/cache/vpk/.git --work-tree=/var/cache/vpk commit -a -m "Revert $*"
+	if test ! -d "$_GIT_DIR"; then
+		try vpkinit $_WORK_DIR
+		log "Initialised."
+	fi
+
+	case "$action" in
+	uninstall)
+		try vpkinstall "$@"
+        log "Installed packages $*."
+		msg="Install $*"
 		;;
-	*)
-		# shellcheck disable=SC2086
-		# shellcheck disable=SC2048
-		apt-get -q --autoremove purge $*
-		dpkg-query -Wf '${Package}=${Version}\n' | sort > /var/cache/vpk/packages
-		git --git-dir /var/cache/vpk/.git --work-tree=/var/cache/vpk commit -a -m "Remove $*"
-		;;
-esac
+	esac
+
+	try vpkcommit "$_WORK_DIR" "$msg"
+}
+
+print_usg() {
+	cat <<'EOF' >&2
+pkutils v0.7.0 (C) Cristian Ariza
+
+Usage: vpkrm [-dv] [--help] [-c COMMITID] [PACKAGE]...
+EOF
+	exit "$1"
+}
+
+try() {
+	if ! "$@"; then
+		exit 1
+	fi
+}
+
+log() {
+	if "$verbose"; then
+		printf "%s\n" "$*"
+	fi
+}
+
+main "$@"
